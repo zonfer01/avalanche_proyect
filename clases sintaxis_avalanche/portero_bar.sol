d8225b3563349738241a7031ff56e9ba3f2d35c1
zonfer01 <dalanfernandoaguirre26@gmail.com>
2024-10-03 23:09:50 -0500

    constructor en el contrato del portero

--- clases sintaxis_avalanche/portero_bar.sol
+++ clases sintaxis_avalanche/portero_bar.sol
@@ -3,6 +3,12 @@
 pragma solidity^ 0.8.24;
 contract portero_bar{
     uint256 public numero; /// variable de estado que almacena la edad
+    uint256 public edad_init;  // se establece la edad limite , para la mayoria de edad
+
+    constructor (uint256 _edad_init){
+        edad_init = _edad_init;
+    }
+
 
     function establecer_numero(uint256 _numero)public {
         numero  =_numero;
@@ -12,14 +18,14 @@ contract portero_bar{
     function mensaje()public view returns (string memory){
 
         // control de flujo if-else
-        if (numero> 18){
+        if (numero>  edad_init){
             return "puedes ingresar porque eres mayor de edad";
 
-        }else if (numero ==18) {   //condicional secundaria
-            return "acabas de cumplir 18, puedes ingresar ";
+        }else if (numero ==edad_init) {   //condicional secundaria
+            return "acabas de cumplir la mayoria de edad, puedes ingresar ";
 
         }else { //condicional final 
-        return "eres menor de edad , no pedes ingresar";
+        return "eres menor de edad , no puedes ingresar";
         
 
 
